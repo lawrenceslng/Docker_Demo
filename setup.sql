b0f79d2d7249ce009ebad3facc18ea40f9515c5f
Lawrence Ng <lawrencesuilunng@gmail.com>
2025-02-03 21:15:07 -0800

    1st attempt at Dockerfile

---
+++ setup.sql
@@ -0,0 +1,13 @@
+-- create the databases
+CREATE DATABASE IF NOT EXISTS test;
+
+USE test;
+
+CREATE TABLE books (
+    id int NOT NULL,
+    title varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    cover varchar(255),
+    price int NOT NULL,
+    PRIMARY KEY (ID)
+)
